f08fa9fbce2aa43a09b40aae7a249ca363fe2e06
Alex Sandri <me@alexsandri.com>
2021-01-17 16:45:16 +0100

    Update formù

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 import { HeaderComponent } from './components/header/header.component';
 
@@ -24,7 +25,8 @@ import { HeaderComponent } from './components/header/header.component';
     MatToolbarModule,
     MatFormFieldModule,
     MatInputModule,
-    MatButtonModule
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
