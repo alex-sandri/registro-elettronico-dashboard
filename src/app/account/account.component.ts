7e1d4dc9e136dc927c695e24fb05cf0a8f2ef343
Alex Sandri <me@alexsandri.com>
2021-01-19 17:39:35 +0100

    Fix: Angular compilation errors

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -10,6 +10,10 @@ export class AccountComponent
 {
   public user = this.auth.user;
 
+  public admin = this.user?.type === "admin" ? this.user : null;
+  public student = this.user?.type === "student" ? this.user : null;
+  public teacher = this.user?.type === "teacher" ? this.user : null;
+
   constructor(private auth: AuthService)
   {}
 }
