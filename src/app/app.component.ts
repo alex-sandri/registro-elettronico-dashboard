d9b5a423bcc8e0f58676d6e5e5cb88c189a1ad88
Alex Sandri <me@alexsandri.com>
2021-01-21 15:56:15 +0100

    Update sidenav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { TRetrieveUserResponseDataType } from './services/api/api.service';
 import { AuthService } from './services/auth/auth.service';
 
 @Component({
@@ -11,12 +12,16 @@ export class AppComponent
 {
   title = 'registro-elettronico-dashboard';
 
-  canLoad = false;
+  public user?: TRetrieveUserResponseDataType;
+
+  public canLoad = false;
 
   constructor (auth: AuthService, router: Router)
   {
     auth.init().then(user =>
     {
+      this.user = user ?? undefined;
+
       if (user && location.pathname === "/")
       {
         router.navigateByUrl("account");
